17300240fa6677234087269d917936f5f86da2a4
Evgeniy <evgeniipusser88@gmail.com>
2025-01-24 01:07:24 +0200

    rotating through js

---
+++ src/animator.js
@@ -0,0 +1,11 @@
+
+
+let earthAnimationElement = document.getElementById("earth_animate");
+
+function start() {
+  earthAnimationElement.classList.add("animation-pop");
+}
+
+function stop() {
+  earthAnimationElement.classList.remove("animation-pop");
+}
